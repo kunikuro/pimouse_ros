8813f6d92523db779da6c6ebf6f209dcc590647f
kunimatsu <asako.kunimatsu@gmail.com>
2020-01-09 11:56:58 +0900

    Add travis_yml travis_ros_install_bash

---
+++ test/travis_ros_install.bash
@@ -0,0 +1,22 @@
+#!/bin/bash -xve
+
+#required packages
+sudo pip install catkin_pkg
+sudo pip install empy
+sudo pip install pyyaml
+sudo pip install rospkg
+
+#ros install
+cd ..
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu16.04_server.git
+cd ./ros_setup_scripts_Ubuntu16.04_server
+bash ./setup0.bash
+bash ./setup1.bash
+
+#catkin setup
+mkdir -p ~/catkin_ws_src
+cd ~/catkin_ws/src
+source /opt/ros/kinetic/setup.bash
+catkin_init_workspace
+cd ~/catkin_ws
+catkin_make
