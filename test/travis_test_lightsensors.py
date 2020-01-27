d9603d8486b0a27fede14db509387c3bf897a9cc
kunimatsu <asako.kunimatsu@gmail.com>
2020-01-27 16:12:58 +0900

    fix8 travis_test_lightsensors 19self.values

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -16,7 +16,7 @@ class LightsensorTest(unittest.TestCase):
         self.values = data
 
     def check_values(self,lf,ls,rs,rf):
-        vs = self.valus
+        vs = self.values
         self.assertEqual(vs.left_forward, lf, "different value : left_forward")
         self.assertEqual(vs.left_side, ls, "different value : left_side")
         self.assertEqual(vs.right_side, rs, "different value : right_side")
