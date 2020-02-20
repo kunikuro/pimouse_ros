db115dc70818a79c73655683eb3d15763c92e0a7
kunimatsu <asako.kunimatsu@gmail.com>
2020-02-20 13:05:29 +0900

    Fix travis_test_motors2.py

--- test/travis_test_motors2.py
+++ test/travis_test_motors2.py
@@ -5,7 +5,7 @@ import rosnode, rospy
 import time
 from pimouse_ros.msg import MotorFreqs
 from geometry_msgs.msg import Twist
-from std.srvs.srv import Trigger TriggerResponse
+from std.srvs.srv import Triggeri, TriggerResponse
 
 class MotorTest(unittest.TestCase):
     def setUp(self):
